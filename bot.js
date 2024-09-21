54addaec91a37b96bbf5cbc081565189a9f91922
proskillowner <proskillowner@gmail.com>
2024-09-21 19:39:41 +0200

    updated bot

--- bot.js
+++ bot.js
@@ -579,7 +579,7 @@ function extractSocials(sourceCode) {
 			} else {
 					return `<a href="${url}" target="_blank">Website</a>`;
 			}
-	}).join('<br/>');
+	}).join('  ');
 }
 
 function printAnalysis(analysis, socials) {

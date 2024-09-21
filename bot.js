b141d7da3c767f82b3f7ea924ee6679a2bc030f6
proskillowner <proskillowner@gmail.com>
2024-09-21 20:10:37 +0200

    add renounce function monotoring

--- bot.js
+++ bot.js
@@ -543,6 +543,8 @@ async function fetchTransactions(contractAddress) {
 function analyzeTransactions(transactions) {
 	const renounceOwnershipCalls = transactions.filter(tx => 
 			tx.input.startsWith('0x79ba5097') // The method ID for renounceOwnership()
+			|| 
+			tx.input.startsWith('0x1f76a7af') // The method ID for renounce() 
 	);
 	return renounceOwnershipCalls.length > 0 ? '🟢 Executed' : '🔴 Never executed';
 }
